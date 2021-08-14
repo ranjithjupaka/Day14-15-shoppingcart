85f1b7b0916d711fb84d0a1060b0345d3506d1ba
ranjithcoder <74724775+ranjithcoder@users.noreply.github.com>
2021-08-14 13:11:01 +0530

    cart dropdown menu functionality added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,74 @@
+import { Button } from 'react-bootstrap'
 import React from 'react'
 import { Container,  Dropdown , Nav ,  FormControl,  Navbar, Badge } from 'react-bootstrap'
+import { AiFillDelete } from 'react-icons/ai'
 import { FaShoppingCart } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { CartState } from '../../context/Context'
+import '../Styles.css'
 
 
 const Header = () => {
 
-    const { state:{cart}} = CartState();
+    const { state:{cart}, dispatch} = CartState();
 
     return (
-        <Navbar bg="dark" variant="dark" style={{height:80}}>
-            <Container>
-                <Navbar.Brand>
-                    <Link to='/'>Shopping Cart</Link>
-                </Navbar.Brand>
-                <Navbar.Text className='search'>
-                 <FormControl 
-                 style={{ width:500 }}
-                 placeholder="search for a product"
-                 className='m-auto'
-                 />  
-                </Navbar.Text>
-                <Nav>
-                    <Dropdown alignRight>
-                        <Dropdown.Toggle variant="success">
-                            <FaShoppingCart color="white" fontSize="25px"/>
-                            <Badge>{cart.length}</Badge>
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu style={{minWidth:370}} >
-                            <span style={{ padding:10  }}>Cart is Empty!</span>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Nav>
-            </Container>
-        </Navbar>
+      <Navbar bg='dark' variant='dark' style={{ height: 80 }}>
+        <Container>
+          <Navbar.Brand>
+            <Link to='/'>Shopping Cart</Link>
+          </Navbar.Brand>
+          <Navbar.Text className='search'>
+            <FormControl
+              style={{ width: 500 }}
+              placeholder='search for a product'
+              className='m-auto'
+            />
+          </Navbar.Text>
+          <Nav>
+            <Dropdown alignRight>
+              <Dropdown.Toggle variant='success'>
+                <FaShoppingCart color='white' fontSize='25px' />
+                <Badge>{cart.length}</Badge>
+              </Dropdown.Toggle>
+              <Dropdown.Menu style={{ minWidth: 370 }}>
+                {cart.length > 0 ? (
+                  <>
+                    {cart.map((prod) => (
+                      <span className='cartitem' key={prod.id}>
+                        <img
+                          src={prod.image}
+                          className='cartItemImg'
+                          alt={prod.name}
+                        />
+                        <div className='cartItemDetail'>
+                          <span>{prod.name}</span>
+                          <span>₹ {prod.price.split('.')[0]}</span>
+                        </div>
+                        <AiFillDelete 
+                            fontSize="20px"
+                            style={{ cursor: 'pointer' }}
+                            onClick={() => dispatch({
+                                type:'REMOVE_FROM_CART',
+                                payload:prod,
+                            })}
+                        />
+                      </span>
+                    ))}
+                    <Link to="/cart">
+                        <Button style={{width:'95%',margin:"0 10px"}}>
+                            Go to Cart
+                        </Button>
+                    </Link>
+                  </>
+                ) : (
+                  <span style={{ padding: 10 }}>Cart is Empty!</span>
+                )}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+        </Container>
+      </Navbar>
     )
 }
 
