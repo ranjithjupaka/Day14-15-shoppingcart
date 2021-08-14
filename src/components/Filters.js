a36675d6434bbf3022944d82853a604efe7259d1
ranjithcoder <74724775+ranjithcoder@users.noreply.github.com>
2021-08-14 10:59:28 +0530

    added rating and products components

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Filters = () => {
+
+    const [rate, setRate] = useState(0)
     return (
       <div className='filters'>
         <span className='title'>Filter Products</span>
@@ -42,9 +46,13 @@ const Filters = () => {
           />
         </span>
         <span>
-            rating
+            <label style={{ paddingRight:10 }} >Rating: </label>
+            <Rating 
+            rating={rate}
+            onClick={(i) => setRate(i+1)} 
+            style={{cursor: 'pointer'}} />
         </span>
-        <Button variant='light'>Clear Filters</Button>
+        <Button variant='light'>Clear Filters</Button> 
       </div>
     )
 }
--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Button, Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const SingleProduct = ({prod}) => {
     return (
-   <div>{prod.name}</div>
+      <div className='products'>
+        <Card>
+          <Card.Img variant='top' src={prod.image} alt={prod.name} />
+          <Card.Body>
+            <Card.Title>{prod.name}</Card.Title>
+            <Card.Subtitle style={{ paddingBottom: 10 }}>
+              <span>₹ {prod.price.split(".")[0]}</span>
+              {prod.fastDelivery ? (
+                  <div>Fast Delivery</div>
+              ):(
+                  <div>4 Days Delivery</div>
+              )}
+              <Rating rating={prod.ratings}/>
+            </Card.Subtitle>
+            <Button disabled={!prod.inStock}>
+            {!prod.inStock ? 'Out of Stock' : "Add to Cart"}
+            </Button>
+            <Button variant="danger">Remove from Cart</Button>
+          </Card.Body>
+        </Card>
+      </div>
     )
 }
 
